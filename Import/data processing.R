c84357e39ef88f97418ceac19b6a3a9c0ce468d8
Jay4869 <jay4869@gmail.com>
2019-07-05 01:00:58 -0400

    update data processing script

--- Import/data processing.R
+++ Import/data processing.R
@@ -7,62 +7,70 @@ library(data.table)
 library(tibble)
 library(tidyr)
 
-base = read.csv("../Data/original/base_number.csv")$x %>% as.character()
+setwd("E:/Work/Taxi")
 
-load = function(i)
+base = read.csv("./Data/base_number.csv")$x %>% as.character()
+
+weather = read.csv("./Data/weather.csv") %>%
+  mutate(date = as.Date(as.character(DATE))) %>%
+  select(-STATION, -TAVG, -DATE)
+
+import_data = function(start, end)
 {
-  # set file path 
-  x = paste("../Data/original/20180", i, ".csv") %>% gsub(" ", "", .)
+  data = c()
+  for(i in start:end)
+  {
+    file = paste("./Data/20180", i, ".csv") %>% gsub(" ", "", .)
+    
+    # temp = fread(file) %>%
+    #   filter(Dispatching_base_number %in% c("B02510", "B02800", base)) %>%
+    #   mutate(type = ifelse(Dispatching_base_number == "B02510", "Lyft",
+    #                        ifelse(Dispatching_base_number == "B02800", "Via", "Uber")),
+    #          pickup_datetime = ymd_hms(Pickup_DateTime),
+    #          dropoff_datetime = ymd_hms(DropOff_datetime),
+    #          travel_time = as.numeric(dropoff_datetime-pickup_datetime),
+    #          date = date(pickup_datetime),
+    #          month = month(pickup_datetime),
+    #          day = day(pickup_datetime),
+    #          pick_hour = hour(pickup_datetime),
+    #          pickup_location_id = PUlocationID,
+    #          dropoff_location_id = DOlocationID) %>%
+    #   select(travel_time, pickup_datetime, dropoff_datetime, date, month, day,
+    #          pick_hour, pickup_location_id, dropoff_location_id, type) %>%
+    #   as_tibble()
+    
+    temp = fread(file) %>% data.table() %>%
+      .[Dispatching_base_number %in% c("B02510", "B02800", base)] %>%
+      .[, type := ifelse(Dispatching_base_number == "B02510", "Lyft",
+                         ifelse(Dispatching_base_number == "B02800", "Via", "Uber"))] %>%
+      .[, pickup_datetime := ymd_hms(Pickup_DateTime)] %>%
+      .[, dropoff_datetime := ymd_hms(DropOff_datetime)] %>%
+      .[, travel_time := as.numeric(dropoff_datetime-pickup_datetime)] %>%
+      .[, date := date(pickup_datetime)] %>%
+      .[, month := month(pickup_datetime)] %>%
+      .[, day := day(pickup_datetime)] %>%
+      .[, pick_hour := hour(pickup_datetime)] %>%
+      .[, pickup_location_id := PUlocationID] %>%
+      .[, dropoff_location_id := DOlocationID] %>%
+      .[, list(travel_time, pickup_datetime, dropoff_datetime, date, month, day,
+               pick_hour, pickup_location_id, dropoff_location_id, type)]
+    
+    temp = temp[, r := row_number(pickup_datetime), by = .(day, pick_hour)][r <= 20]
+
+    data = rbindlist(list(data, temp), use.names = FALSE)
+  }
   
-  # load orginal data
-  # identify type
-  # remove useless columns
-  temp = fread(x) %>%
-    filter(Dispatching_base_number %in% c("B02510", "B02800", base)) %>%
-    mutate(type = ifelse(Dispatching_base_number == "B02510", "Lyft",
-                         ifelse(Dispatching_base_number == "B02800", "Via", "Uber"))) %>%
-    select(-SR_Flag, -Dispatching_base_number, -Dispatching_base_num) %>%
-    as.tibble()
+  remove(temp)
   
-  # export subset monthly data as back-up
-  a = paste("../Data/tables/", i, ".csv") %>% gsub(" ", "", .)
-  write.csv(temp, a)
-  
+  data = data[weather, on = 'date', nomatch = 0][, r:=NULL]
+  fwrite(data, paste("./Data/", start, "-", end, "_weather_mini.csv") %>% gsub(" ", "", .))
+
   return(data)
 }
 
-data = c()
-for(i in 1:12)
-{
-  # load original monthly data
-  temp = load(i)
-  
-  # combine data
-  data = bind_rows(data, temp)
-  
-  # optimize memory usage
-  remove(temp)
-}
+# import_data(1,6) %>% as_tibble() %>%
+#   group_by(day, pick_hour) %>%
+#   filter(row_number(pickup_datetime) <= 300)%>%
+#   fwrite("./Data/sampled_1-6.csv")
 
-# re-format data types
-data = data %>%
-  mutate(pick = ymd_hms(Pickup_DateTime), drop = ymd_hms(DropOff_datetime), duration = as.numeric(drop - pick)/60) %>%
-  select(-V1, -Pickup_DateTime, -DropOff_datetime)
-
-# only for Dec due to different time format
-data2 = as.tibble(fread("./Data/tables/12.csv")) %>%
-  mutate(pick = mdy_hm(Pickup_DateTime), drop = mdy_hm(DropOff_datetime), duration = as.numeric(drop - pick)/60) %>%
-  select(-V1, -Pickup_DateTime, -DropOff_datetime)
-
-# load weather dataset
-weather = read.csv("./Data/original/weather.csv") %>%
-  mutate(DATE = as.Date(as.character(DATE))) %>%
-  select(-STATION, -TAVG, -TMAX, -TMIN, -AWND) %>%
-  as.tibble()
-
-# Combining weather and trips
-data %>%
-  mutate(DATE = as.Date(pick)) %>%
-  group_by(DATE) %>%
-  summarise(m.d = median(duration)) %>%
-  inner_join(. , weather, by = "DATE")
+data = import_data(1,12)
